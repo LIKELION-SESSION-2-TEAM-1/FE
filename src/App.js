39909ae64a13df27530add2296e3067464014c2a
bbungbbaeng <hobakggulmatna@gmail.com>
2025-10-10 11:21:18 +0900

    (feat) 리액트 디렉토리 설정

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './assets/css/styles.module.css';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+    return (
+        <>
+        </>
+    );
+}
+
+export default App;
