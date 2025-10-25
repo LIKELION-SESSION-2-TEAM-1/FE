98ae76c8c02aa1631cc860d9fca2809aca308ac8
yunjung0 <pyjlily@naver.com>
2025-10-25 16:33:42 +0900

    메인페이지 수정1

--- src/pages/OnBoarding/Home/Home.jsx
+++ src/pages/OnBoarding/Home/Home.jsx
@@ -1,11 +1,20 @@
 import styles from './Home.module.css';
 import Header from '../../../components/Header/Header';
+import pic from "../../../assets/pic/pic1.png";
+
 
 const Home = () => {
     return (
         <div className={styles.main__wrapper}>
             <div className={styles.main__container}>
+                <div className={styles.main__card}>
+                    <div className={styles.card__text}>
+                        <p className={styles.card__text1}>막막했던 여행,<br />대화로 시작하세요.</p>
+                        <p className={styles.card__text2}>친구들을 초대해서 대화만으로 계획을 완성하세요.</p>
+                    </div>
+                    <img src={pic} className={styles.card__pic} />
                     
+                </div>
             </div>
         </div>
     )
