b5f873a716d5eff81bf0fa9467eeb07002416c28
bbungbbaeng <hobakggulmatna@gmail.com>
2025-10-25 14:21:30 +0900

    (refactor) 홈 화면 CSS 수정

--- src/pages/OnBoarding/Home/Home.jsx
+++ src/pages/OnBoarding/Home/Home.jsx
@@ -3,6 +3,11 @@ import styles from './Home.module.css';
 const Home = () => {
     return (
         <>
+            <div className={styles.main__wrapper}>
+                <div className={styles.main__container}>
+                    <div className={styles.background} />
+                </div>
+            </div>
         </>
     )
 }
