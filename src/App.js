74e388482aa231a392bde92bb1711de0ed72104b
yunjung0 <pyjlily@naver.com>
2025-10-25 14:27:04 +0900

    로고 추가

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     return (
         <>
             <Routes>
+                <Route path="/" element={<Header />} />
                 <Route path="/" element={<Home />} />
             </Routes>
         </>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import styles from './Header.module.css';
+import React from "react";
+import logo from '../../assets/pic/TokPlan.png'
 
 const Header = () => {
     return (
-        <>
-        </>
+        <div className='logo'>
+            <img src={logo}></img>
+        </div>
     )
 }
 
