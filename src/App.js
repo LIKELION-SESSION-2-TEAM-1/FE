b955a134fd0bacd47c2155586ea8b5c9d5afbf5d
bbungbbaeng <hobakggulmatna@gmail.com>
2025-10-25 13:46:26 +0900

    (feat) 리액트 파일 구성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './assets/css/styles.module.css';
 import { Route, Routes } from 'react-router-dom';
 
+import Header from './components/Header/Header.jsx'
+import Home from './pages/OnBoarding/Home/Home';
+
 function App() {
     return (
         <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
         </>
     );
 }
